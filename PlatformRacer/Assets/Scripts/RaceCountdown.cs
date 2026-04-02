2afeb96eef9c56cd3a740fcf5a1ddafca2d7ad89
Kaden Goodrid <kgoodrid23@gmail.com>
2026-04-01 22:01:23 -0500

    Added background music to countdown

--- PlatformRacer/Assets/Scripts/RaceCountdown.cs
+++ PlatformRacer/Assets/Scripts/RaceCountdown.cs
@@ -7,6 +7,7 @@ public class RaceCountdown : MonoBehaviour
     public TMP_Text countdownText;
     public AudioClip countdownBeep;
     public AudioClip goSound;
+    public AudioClip backgroundMusic;
     private AudioSource audioSource;
     private bool hasStarted = false;
 
@@ -44,5 +45,9 @@ public class RaceCountdown : MonoBehaviour
         yield return new WaitForSeconds(1f);
 
         countdownText.gameObject.SetActive(false);
+
+        audioSource.clip = backgroundMusic;
+        audioSource.loop = true;
+        audioSource.Play();
     }
 }
